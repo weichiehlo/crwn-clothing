0ff2e47e4367e757ee599746267588eb31d1f90c
Andy <wlo@fortinet.com>
2020-06-15 17:49:47 -0700

    carticon and cart drop down to to our application including redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '..//../firebase/firebase.utils'
-
 import { connect } from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ( { currentUser } )=>(
+const Header = ( { currentUser, hidden } )=>(
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -26,12 +27,19 @@ const Header = ( { currentUser } )=>(
                     SINGIN
                 </Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden? null:<CartDropdown/>
+            
+        }
+        
     </div>
 )
 
-const mapStateToProps = state  =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}})  =>({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
