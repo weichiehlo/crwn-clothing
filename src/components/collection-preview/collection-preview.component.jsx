fde0378c59850f5fc460b44865a30b0117b1866a
Andy <wlo@fortinet.com>
2020-06-16 14:48:06 -0700

    updated shop data with new data updated cart utils to group items to cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const CollectionPreview = ({ title,items })=>(
             {
                 items
                 .filter((item,idx) => idx < 4)
-                .map(({id, ...theRest})=>(
-                    <CollectionItem key={id} {...theRest}/>
+                .map((item)=>(
+                    <CollectionItem key={item.id} item={item}/>
                 ))
             }
         </div>
