7562c3af309e09850e68b1235c3e3425d86b8b85
Andy <wlo@fortinet.com>
2020-06-19 16:12:11 -0700

    updating our app with reselect replace cart selector with memoize selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import './cart-dropdown.styles.scss'
 
@@ -19,7 +20,7 @@ const CartDropdown = ({ cartItems })=>(
     </div>
 )
 
-const mapStatesToProps = ({cart:{cartItems}})=>({
-    cartItems
+const mapStatesToProps = (state)=>({
+    cartItems:selectCartItems(state)
 })
 export default connect(mapStatesToProps)(CartDropdown);
