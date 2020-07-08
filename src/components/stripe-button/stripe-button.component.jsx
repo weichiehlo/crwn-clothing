16a3ed28f2bc9814fd2bbd3e0823ce50d17b961c
Andy <wlo@fortinet.com>
2020-07-08 13:48:29 -0700

    fix svg typo

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -15,7 +15,7 @@ const StripeCheckoutButton = ( {price} )=>{
         name='CRWN Clothing Ltd'
         billingAddress
         shippingAddress
-        image='https://svgshare.com/i/CUz/svg'
+        image='https://svgshare.com/i/CUz.svg'
         description={`Your total is $${price}`}
         amount={priceForStripe}
         panelLabel='Pay Now'
